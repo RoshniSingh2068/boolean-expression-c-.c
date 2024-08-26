3e5d2c8bd64d252988e0fc778648a6ed7f790e9a
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 11:25:03 +0530

    Create boolean expression c++.c

---
+++ boolean expression c++.c
@@ -0,0 +1,12 @@
+// A Boolean expression return a boolean value either ( true ) or ( false )
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    int x = 5;
+    int y = 6;
+    cout << (x < y );
+    return 0;
+
+}
